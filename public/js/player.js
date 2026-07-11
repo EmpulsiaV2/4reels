c2a9e9a9ed08986ed38a12ab04a0b0167091821b
Logan <driffiti@gmail.com>
2026-07-11 21:33:46 +0100

    Change from localstorage to Database

--- public/js/player.js
+++ public/js/player.js
@@ -49,10 +49,10 @@ class Player {
             <div class="pspinner"></div>
             <span class="povl-txt">Loading stream…</span>
           </div>
-          <iframe
-            id="pif-${this.id}"
-            allow="fullscreen *; autoplay *; picture-in-picture *; encrypted-media *"
-            style="position:absolute;inset:0;width:100%;height:100%;border:none;background:#000"
+          <iframe 
+            id="pif-${this.id}" 
+            allow="fullscreen *; autoplay *; picture-in-picture *; encrypted-media *" 
+            style="position:absolute;inset:0;width:100%;height:100%;border:none;background:#000" 
             src="about:blank">
           </iframe>
         </div>
